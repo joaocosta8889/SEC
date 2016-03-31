85a7fb58af70b7224d40f26fa441c944766ff7ed
peress <ist173831@tecnico.ulisboa.pt>
2016-03-31 12:05:56 +0100

    Changes on Block. Couldn't fix the bug yet

--- FS-Server/src/fs/Block.java
+++ FS-Server/src/fs/Block.java
@@ -34,16 +34,19 @@ public class Block {
 		this.signature = signature;
 	}
 	
-	private byte[] writeBlock(Data cont){
+	// voltar a tornar private <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	public byte[] writeBlock(Data cont){
 		
 		byte[] newContent = null;
 		byte[] toWrite = cont.getDataContent();
+		int dataPos = cont.getDataPos();
+		int dataPosSize = dataPos + cont.getDataSize();
 		
-		if((cont.getDataPos() + cont.getDataSize()) > this.content.length){
+		if(( dataPos + cont.getDataSize()) > this.content.length){
 			// cria um novo vetor e vai copiar conteudo
 			int contSize = this.content.length;
 			//int newSize =  contSize + ((cont.getDataPos() + cont.getDataSize()) - contSize);
-			int newSize =  contSize + (cont.getDataPos() + cont.getDataSize());
+			int newSize =  contSize + dataPosSize;
 			newContent = new byte[newSize];
 			
 			// copia conteudo antigo
@@ -52,13 +55,13 @@ public class Block {
 			}
 			
 			// copia novo conteudo 
-			for(int i=this.content.length, j=0; i<newSize || j < cont.getDataSize(); i++, j++){
+			for(int i=dataPos, j=0; i< dataPosSize; i++, j++){
 				newContent[i] = toWrite[j];
 			}
 		}else{
 			//escreve por cima 
 			
-			for(int i=cont.getDataPos(), j=0; i<cont.getDataSize() || j < cont.getDataSize(); i++, j++){
+			for(int i=dataPos, j=0; i< dataPosSize; i++, j++){
 				this.content[i] = toWrite[j];
 			}
 			newContent = this.content;
--- FS-Server/src/tests/TestBlock.java
+++ FS-Server/src/tests/TestBlock.java
@@ -1,11 +1,26 @@
-package tests; 
+package tests;
+
+import fs.Block;
+import fs.Data;
 
 public class TestBlock {
-	
-	@Test
-	public void TestBlock1(){
-		Data d1 = new Data();
+	/*
+	public static void main(String[] args) throws Exception {
 		
-	}
+		byte[] firstEx = "Rui".getBytes();
+		byte[] secondEx = "Peresidente".getBytes();
+		
+		Data d1 = new Data(firstEx.length, 0, firstEx);
+		Data d2 = new Data(secondEx.length, 2, secondEx);
+		
+		Block b1 = new Block();
+		b1.writeBlock(d1);
+		
+		System.out.println(">>>>> B1 <<<<<<");
+		byte[] b1cont = b1.getBlockContent();
+		for(int i= 0; i < b1cont.length; i++) {
+			System.out.println("" + b1cont[i]);
+		}
+	}*/
 
 }
